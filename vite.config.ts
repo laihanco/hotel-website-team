e0011afb92cd7c909d9438691497ac74ef210399
Madao <gn00678465@gmail.com>
2023-12-29 05:47:43 +0800

    chore: 新增 husky & lint-staged 設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import postcss from './postcss.config.js';
+
+const postcss = require('./postcss.config.js');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
-  css:{
-    postcss
+  css: {
+    postcss,
   },
-})
+});
