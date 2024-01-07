c6f971d04c1bdb96b8bdd83259c8f3c76ec3a889
Han <>
2024-01-08 00:06:53 +0800

    fix(base-nav): 手機版選單點擊後未關閉
    原因：關閉函式程式碼未完成、a 標籤寬度不足

--- src/components/layout/BaseNav.vue
+++ src/components/layout/BaseNav.vue
@@ -4,35 +4,35 @@
   >
     <img src="@/assets/LOGO.svg" alt="LOGO" class="w-[110px] desktop:w-[198px]" />
 
-    <!-- 手機菜單按鈕 -->
+    <!-- 手機-選單開啟鈕 -->
     <icMenu class="h-[40px] w-[40px] cursor-pointer p-2 desktop:hidden" @click="showMenu = true" />
 
-    <!-- 手機菜單內容 -->
+    <!-- 手機-選單內容 -->
     <div
       v-show="showMenu"
       class="absolute left-0 top-0 h-full w-full bg-neutral-black p-5 desktop:hidden"
     >
-      <ul class="flex h-full flex-col justify-center gap-4 text-center" @click="goPage">
+      <ul class="flex h-full flex-col justify-center gap-4 text-center" @click="closeMenu">
         <li>
-          <router-link class="cursor-pointer p-4" to="/rooms">客房旅宿</router-link>
+          <router-link class="w-full cursor-pointer p-4" to="/rooms">客房旅宿</router-link>
         </li>
         <li>
-          <router-link class="cursor-pointer p-4" to="/login">會員登入</router-link>
+          <router-link class="w-full cursor-pointer p-4" to="/login">會員登入</router-link>
         </li>
         <li class="rounded-lg bg-primary-100">
-          <router-link class="cursor-pointer p-4" to="/booking"> 立即訂房 </router-link>
+          <router-link class="w-full cursor-pointer p-4" to="/booking"> 立即訂房 </router-link>
         </li>
       </ul>
     </div>
 
-    <!-- 手機菜單關閉鈕 -->
+    <!-- 手機-選單關閉鈕 -->
     <icClose
       v-show="showMenu"
       class="absolute right-5 top-5 h-[48px] w-[48px] cursor-pointer p-2"
       @click="showMenu = false"
     />
 
-    <!-- 桌機菜單內容 -->
+    <!-- 桌機-選單內容 -->
     <ul class="hidden items-center gap-4 desktop:flex">
       <li>
         <router-link class="cursor-pointer p-4" to="/rooms">客房旅宿</router-link>
@@ -48,14 +48,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, Ref } from 'vue';
 import icMenu from '@/assets/icons/icMenu.vue';
 import icClose from '@/assets/icons/icClose.vue';
 
-const showMenu: boolean = ref(false);
+const showMenu: Ref<boolean> = ref(false);
 
-const goPage = (e: PointerEvent) => {
-  console.log(e);
+const closeMenu = (e: PointerEvent) => {
+  if ((e.target as HTMLElement).nodeName === 'A') showMenu.value = false;
 };
 </script>
 
